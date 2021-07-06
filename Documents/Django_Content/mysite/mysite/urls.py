93a8a288ffb49df8a13fe892f8ce5ec195f539b3
asandyy <sandyli110@gmail.com>
2021-07-06 12:26:15 -0700

    updating lab

--- Documents/Django_Content/mysite/mysite/urls.py
+++ Documents/Django_Content/mysite/mysite/urls.py
@@ -19,13 +19,7 @@ from polls import views
 
 app_name = 'polls'
 urlpatterns = [
- # ex: /polls/
-    path('', views.index, name='index'),
+    path('polls/', include("polls.urls")),
     path('admin/', admin.site.urls),
-# ex: /polls/5/
-    path('<int:question_id>/', views.detail, name='detail'),
-# ex: /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
-# ex: /polls/5/vote/
-    path('<int:question_id>/vote/', views.vote, name='vote'),
+    
 ]
--- Documents/Django_Content/mysite/polls/admin.py
+++ Documents/Django_Content/mysite/polls/admin.py
@@ -2,6 +2,5 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Question, Choice
-
 admin.site.register(Question)
 admin.site.register(Choice)
